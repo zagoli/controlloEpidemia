56cea28b978364d16febe31f1d9c14cf7cce2019
jacopo <poipoi.jacopo@hotmail.com>
2020-05-08 15:35:04 +0200

    creato db e collegamento

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,8 +10,8 @@ import javax.persistence.metamodel.EntityType;
 import java.util.Map;
 
 public class Main {
-    private static final SessionFactory ourSessionFactory;
 
+    private static final SessionFactory ourSessionFactory;
     static {
         try {
             Configuration configuration = new Configuration();
