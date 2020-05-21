fdd5913a8ecabf51c9f974943ca9e43cbf2aa276
Giuseppe Brusco <giuseppe.brusco@yahoo.it>
2020-05-21 16:52:16 +0200

    example csv in import data

---
+++ src/main/java/com/jgg/controlloEpidemia/importData/ImportData.java
@@ -0,0 +1,38 @@
+package com.jgg.controlloEpidemia.importData;
+
+import com.jgg.controlloEpidemia.service.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
+
+public class ImportData {
+
+    ComuneService comuneService = new ComuneService();
+    DecessiAnnualiService decessiAnnualiService = new DecessiAnnualiService();
+    MalattieSettimanaliService malattieSettimanaliService = new MalattieSettimanaliService();
+    PermessiService permessiService = new PermessiService();
+    ProvinciaService provinciaService = new ProvinciaService();
+    RegioneService regioneService = new RegioneService();
+    RuoloService ruoloService = new RuoloService();
+    TipoTerritorioService tipoTerritorioService = new TipoTerritorioService();
+    UtenteService utenteService = new UtenteService();
+
+    public void importExample() throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(Objects.requireNonNull(classLoader.getResource("directory")).getFile());
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        String row = bufferedReader.readLine();
+        while (!row.equals("")) {
+            String[] vector = row.split(";");
+            //metti la classe dao che ti serve
+            //ExampleService.save(new Example(Integer.parseInt(vector[0]), vector[1]));
+            row = bufferedReader.readLine();
+        }
+        bufferedReader.close();
+    }
+
+
+}
